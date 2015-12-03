9fbeaf13ae8ffae836cee943bbe27c643792944b
Adam <wonton916@gmail.com>
2015-12-03 10:46:58 -0800

    initial files

---
+++ app/src/main/java/com/example/guest/foodwastagereduction/FoodWastageReduction.java
@@ -0,0 +1,13 @@
+package com.example.guest.foodwastagereduction;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FoodWastageReduction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_wastage_reduction);
+    }
+}
